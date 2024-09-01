bf3805a401186dd5cb305df1c213aa5c9aaa98e2
leomsa <leo.akimim@gmail.com>
2024-09-01 19:12:51 -0300

    Refactor

--- solutio/src/main/java/com/solutio/solutio/entities/Product.java
+++ solutio/src/main/java/com/solutio/solutio/entities/Product.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Long id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
