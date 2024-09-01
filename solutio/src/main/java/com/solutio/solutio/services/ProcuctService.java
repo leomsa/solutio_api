d5a2057852ffc16a34c20eda0a7224a77e36b5bd
leomsa <leo.akimim@gmail.com>
2024-09-01 20:01:32 -0300

    Created method to get productByID.

--- solutio/src/main/java/com/solutio/solutio/services/ProcuctService.java
+++ solutio/src/main/java/com/solutio/solutio/services/ProcuctService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProcuctService {
@@ -19,6 +20,10 @@ public class ProcuctService {
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
     }
 
     public void deleteProduct(Long id) {
