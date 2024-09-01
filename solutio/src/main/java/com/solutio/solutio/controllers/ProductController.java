e02685e0b74bfc1da61cc1e61c6fd580332f9cbe
leomsa <leo.akimim@gmail.com>
2024-09-01 19:43:32 -0300

    Refactor

--- solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
+++ solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
@@ -2,17 +2,12 @@ package com.solutio.solutio.controllers;
 
 import com.solutio.solutio.entities.Product;
 import com.solutio.solutio.repositories.ProductRepository;
-import com.solutio.solutio.services.ProcuctService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.swing.text.html.Option;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -21,7 +16,7 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @PostMapping
+    @PostMapping("/product")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         Product saveProduct = productRepository.save(product);
         return ResponseEntity.ok(saveProduct);
