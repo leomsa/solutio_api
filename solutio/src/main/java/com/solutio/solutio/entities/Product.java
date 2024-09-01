242b89d64b2e71a9945c6418a465464cc60789d4
leomsa <leo.akimim@gmail.com>
2024-09-01 19:08:11 -0300

    Created Product class.

--- solutio/src/main/java/com/solutio/solutio/entities/Product.java
+++ solutio/src/main/java/com/solutio/solutio/entities/Product.java
@@ -1,4 +1,28 @@
 package com.solutio.solutio.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String supplier;
+
+    @Column(nullable = false)
+    private Double price;
 }
