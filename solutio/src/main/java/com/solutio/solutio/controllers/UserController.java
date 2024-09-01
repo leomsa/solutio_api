2b98ad490a0c240bf35565f164a2fcc728d5158a
leomsa <leo.akimim@gmail.com>
2024-09-01 20:43:58 -0300

    Optimized imports in all project.

--- solutio/src/main/java/com/solutio/solutio/controllers/UserController.java
+++ solutio/src/main/java/com/solutio/solutio/controllers/UserController.java
@@ -4,9 +4,10 @@ import com.solutio.solutio.entities.User;
 import com.solutio.solutio.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
--- solutio/src/main/java/com/solutio/solutio/repositories/UserRepository.java
+++ solutio/src/main/java/com/solutio/solutio/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.solutio.solutio.repositories;
 
 import com.solutio.solutio.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
