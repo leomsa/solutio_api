3fc731d7cee9ccf0dff9d20bdf63cd97159bc67b
leomsa <leo.akimim@gmail.com>
2024-09-01 19:56:07 -0300

    Created method to getAllProducts.

--- solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
+++ solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
@@ -4,10 +4,9 @@ import com.solutio.solutio.entities.Product;
 import com.solutio.solutio.services.ProcuctService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -20,6 +19,12 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         Product saveProduct = productService.saveProduct(product);
         return ResponseEntity.ok(saveProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
     }
 
 }
