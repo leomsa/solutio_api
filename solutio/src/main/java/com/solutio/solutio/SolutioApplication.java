7190b6d4133077e7fae4eea3a1ba81e6ba393e3f
leomsa <leo.akimim@gmail.com>
2024-09-01 18:19:35 -0300

    Added MySQL configuration.

---
+++ solutio/src/main/java/com/solutio/solutio/SolutioApplication.java
@@ -0,0 +1,13 @@
+package com.solutio.solutio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutioApplication.class, args);
+	}
+
+}
---
+++ solutio/src/test/java/com/solutio/solutio/SolutioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutio.solutio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
