a3968a9a935739b8c18e0bcd5ab3b6aabf167d37
leomsa <leo.akimim@gmail.com>
2024-09-01 18:44:36 -0300

    Refactor UserRepository.

--- solutio/src/main/java/com/solutio/solutio/repositories/User.java
+++
@@ -1,9 +0,0 @@
-package com.solutio.solutio.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface User extends JpaRepository<User, Long> {
-    Optional <User> findByUsername(String username);
-}
---
+++ solutio/src/main/java/com/solutio/solutio/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.solutio.solutio.repositories;
+
+import com.solutio.solutio.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
