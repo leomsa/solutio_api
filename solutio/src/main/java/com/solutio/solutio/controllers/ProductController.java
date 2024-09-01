0ef412e16625fd85cd5dc57dc9d323de2f2bae0c
leomsa <leo.akimim@gmail.com>
2024-09-01 19:38:02 -0300

    Created Method to recovery all products registered.

--- solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
+++ solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package com.solutio.solutio.controllers;
 
 import com.solutio.solutio.entities.Product;
+import com.solutio.solutio.repositories.ProductRepository;
 import com.solutio.solutio.services.ProcuctService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +10,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
     @Autowired
-    private ProcuctService procuctService;
+    private ProductRepository productRepository;
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product saveProduct = procuctService.saveProduct(product);
+        Product saveProduct = productRepository.save(product);
         return ResponseEntity.ok(saveProduct);
     }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
 
 }
