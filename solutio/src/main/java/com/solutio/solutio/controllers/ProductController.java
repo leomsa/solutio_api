00a4ef7514d336e066740c3b619ea59e86b258ca
leomsa <leo.akimim@gmail.com>
2024-09-01 20:01:55 -0300

    Created method to get productByID.

--- solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
+++ solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -25,6 +26,12 @@ public class ProductController {
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.getAllProducts();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
