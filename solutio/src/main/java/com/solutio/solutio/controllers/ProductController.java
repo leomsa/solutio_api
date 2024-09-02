21e98f4add040fea7f42d306f712c1a94ff3185c
leomsa <leo.akimim@gmail.com>
2024-09-02 14:01:37 -0300

    Solved cors from 4002 issue.

--- solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
+++ solutio/src/main/java/com/solutio/solutio/controllers/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
