7866ac52133b428d5a8e5a131733bd98a3a6f104
Leonardo Machado dos Santos <leonardo.machadodossantos@agcocorp.com>
2024-09-03 16:21:04 -0300

    Created integration with fe login.

--- solutio/src/main/java/com/solutio/solutio/controllers/UserController.java
+++ solutio/src/main/java/com/solutio/solutio/controllers/UserController.java
@@ -3,12 +3,12 @@ package com.solutio.solutio.controllers;
 import com.solutio.solutio.entities.User;
 import com.solutio.solutio.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,5 +19,19 @@ public class UserController {
     public ResponseEntity<User> registerUser(@RequestBody User user) {
         User savedUser = userService.savedUser(user);
         return ResponseEntity.ok(savedUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        String email = user.getEmail();
+        String password = user.getPassword();
+
+        Optional<User> foundUser = userService.getUserByEmail(email);
+
+        if (foundUser != null && foundUser.get().getPassword().equals(password)) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        }
     }
 }
